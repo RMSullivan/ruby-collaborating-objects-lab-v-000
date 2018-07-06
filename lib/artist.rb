ecc24d5d19a2a864dd6f59b4376694fa3d2e8049
Rita Sullivan <ritarojas@mac.com>
2018-07-06 15:09:10 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,18 +1,17 @@
 class Artist
   @@all = []
-  attr_accessor :name, :songs
+  attr_accessor :name
 
   def initialize(name)
     @name = name
-    @songs = []
   end
 
   def self.all
     @@all
   end
 
-  def add_song(song)
-    @songs << song
+  def songs
+    Song.find_by_artist(self)
   end
 
   def self.find_or_create_by_name(name)
